4550c54c308b84af541d0e1cb975a55e458f9151
MJ <toja@example.com>
2023-03-17 15:12:09 +0100

    17-03-2023

---
+++ kcppKviCar/CMakeFiles/CarFactory.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CarFactory.dir/src/CarFactory.cxx.o"
+  "CMakeFiles/CarFactory.dir/src/CarFactory.cxx.o.d"
+  "CMakeFiles/CarFactory.dir/src/KviCar.cxx.o"
+  "CMakeFiles/CarFactory.dir/src/KviCar.cxx.o.d"
+  "CMakeFiles/CarFactory.dir/src/KviVan.cxx.o"
+  "CMakeFiles/CarFactory.dir/src/KviVan.cxx.o.d"
+  "CarFactory"
+  "CarFactory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CarFactory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ kcppKviCar/CMakeFiles/CarFactory.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CarFactory.
+# This may be replaced when dependencies are built.
